f850a47fd141adc54bb070356b0206eb1b407263
Slansh <bsousaperes@gmail.com>
2024-08-14 14:40:45 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ function verificarChute() {
     
     if(chute == numeroSecreto) {
         exibirTextoNaTela('h1', 'Acertou!');
-        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentaiva';
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentiva';
         let mensagemTentativas = `Você descrobriu o número secreto com ${tentativas} ${palavraTentativa}!`
         exibirTextoNaTela('p', mensagemTentativas);
         document.getElementById('reiniciar').removeAttribute('disabled')
